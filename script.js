1a0642a09d9b30e2631824a62c85d6095bfd7d66
FilipKrysiak <39153502+FilipKrysiak@users.noreply.github.com>
2018-06-20 14:48:36 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+alert('Witaj w szybkim kalkulatorze obliczania dat podawania leków zwierzętom.\n\rProgram obliczy datę podania 3-ech najbliższych dawek.');
+let drugDate = 0;
+let drugTab = [];
+pobierzDate();
+
+function pobierzDate(){
+drugDate = prompt('Podaj datę poprzedniej dawki leku','dzień-miesiąc-rok');
+if (drugDate != null){
+drugTab = drugDate.split("-");
+for (let i =0; i < drugTab.length; i++){
+  drugTab[i] = parseInt(drugTab[i], 10);
+};
+if (drugTab[0] > 0 && drugTab[0] < 32 && drugTab[1] > 0 && drugTab[1] < 13 && drugTab[2] > 1999 && drugTab[2] < 10000){
+  pobierzInterwal();
+}
+else {
+  alert('Nieprawidłowa data, spróbuj ponownie.');
+  pobierzDate();
+}}
+else {alert('Dziękuję za skorzystanie z programu.')}
+};
+
+function pobierzInterwal(){
+  let interwal = prompt('Podaj interwał dla dawki leku w tygodniach');
+  interwal = interwal * 7;
+  let dateCount = new Date();
+  let dateResult = [];
+  dateCount.setYear(drugTab[2]);
+  dateCount.setMonth(drugTab[1]-1);
+  dateCount.setDate(drugTab[0]);
+  console.log(dateCount);
+  dateCount.setDate(dateCount.getDate() + interwal);
+  for(let i = 0; i<3; i++){
+  dateResult[i] = String(dateCount.getDate()) + '-' + String(dateCount.getMonth() + 1) + '-' + String(dateCount.getYear() + 1900);
+  dateCount.setDate(dateCount.getDate() + interwal);
+  }
+  if (confirm('Kolejne daty podania leku to:\n\r\n\r' + dateResult[0] + '\n\r\n\r' + dateResult[1] + '\n\r\n\r' + dateResult[2] + '\n\r\n\rCzy chcesz sprawdzic kolejny lek?')){
+    pobierzDate();
+  }
+  else { alert('Dziękuję za skorzystanie z programu.')}
+};
